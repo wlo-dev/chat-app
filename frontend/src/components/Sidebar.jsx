55995ea51259f8a0cf22b115c9d20b0a11c96f55
wlo-dev <chagulukawillouby0@gmail.com>
2026-05-27 12:51:52 +0200

    Some Side Bar Updates

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,62 @@
+import { useEffect } from "react";
+import {useChatStore} from "../store/useChatStore";
+import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+ const { getUsers, users, selectUser, setSelectedUser, isUsersLoading, selectedUser}=useChatStore()
 
+ const onlineUsers = [];
+
+ useEffect(() => {
+  getUsers()
+
+ }, [getUsers])
+
+if(isUsersLoading) return <SidebarSkeleton />
+
+return (
+    <aside className= "flex flex-col w-20 h-full transition-all duration-200 border-r lg:w-72 border-base-300">
+      <div className="w-full p-5 border-b border-base-300">
+    <div className="flex items-center gap-2 ">
+      <Users className="size-6" />
+      <span className="hidden font-medium lg:block">Contacts</span>
+    </div>
+{/* Online toggle filter */}
+</div>
+ <div className="w-full py-3 overflow-y-auto">
+  {users.map((user) => (
+    <button
+    key={user._id}
+    onclick={() => selectUser(user)}
+    className={`w-full p-3 flex items-center gap-3 hover:bg-base-300 transition-colors
+      ${selectedUser?._id === user.id ? "bg-base-300 ring-1 ring-base-300" : ""}
+      `}
+      >
+        <div className="relative mx-auto lg:mx-0">
+          <img
+          src={user.profilePic || "/avatar.png"}
+          alt={user.name}
+          className="object-cover rounded-full size-12"
+          />
+          {onlineUsers.includes(user._id) && (
+            <span
+            className="absolute bottom-0 right-0 bg-green-500 rounded-full size-3 ring-2 ring-zinc-900"
+            />
+
+          )}
+        </div>
+        {/* This is the user info and its only visible to the users */}
+        <div className="hidden min-w-0 text-left lg:block">
+        <div className="font-medium truncate ">{user.fullName}</div>
+        <div className="text-sm text-zinc-400">
+          {onlineUsers.includes(user._id) ? "Online" : "Offline"}
+        </div>
+        </div>
+      </button>
+  ))}
+
+ </div>
+    </aside>
+)
+
+};
 export default Sidebar;
