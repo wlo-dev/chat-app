96bbb396965a505f5b5e2193a4ca4cd137af301a
wlo-dev <chagulukawillouby0@gmail.com>
2026-06-08 19:19:50 +0200

    finalising chat header code

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,4 +1,38 @@
-import 
+import { X } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
+import {useChatStore } from "../store/useChatStore";
+
+const ChatHeader = () => {
+  const { selectedUser, setSelectedUser } = useChatStore();
+  const {onlineUsers} = useAuthStore();
+
+  return (
+    <div className="p-2.5 border-b border-base-300">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          {/* Avatar */}
+          <div className="avatar">
+            <div className="relative rounded-full size-10">
+              <img src={selectedUser.profilePic || "avatar.png"} alt={selectedUser.fullName} />
+            </div>
+          </div>
+
+{/* User Info */}
+          <div>
+<h3 className="font-medium">{selectedUser.fullName}</h3>
+<p className="text-sm text-base-content/70">
+  {onlineUsers.includes(selectedUser.id) ? "Online" : "Offline"}
+</p>
+
+
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  )
+} 
 
 
 export default ChatHeader;
