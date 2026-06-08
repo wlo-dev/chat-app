6b27cc583b10d803dc09e0cf94fa68c20e455a7d
wlo-dev <chagulukawillouby0@gmail.com>
2026-06-08 18:59:39 +0200

    imported chat header and message imput

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,36 @@
+import { useChatStore } from "../store/useChatStore";
+import { useEffect} from "react";
+
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+
+
+
+
 const ChatContainer = () => {
+
+    const { messages, getMessages, isMessagesLoading, selectedUser} = useChatStore()
+
+      useEffect(() => {
+        getMessages(selectedUser._id)
+      },[selectedUser, getMessages])
+
+  if(isMessagesLoading) return <div>Loading messages...</div>
+
+
+ 
+
   return (
-    <div>ChatContainer</div>
+    <div className= "flex flex-col flex-1 h-full">
+      <ChatHeader/>
+      <p>messages...</p>
+
+      <MessageInput />
+
+
+    </div>
   )
-}
+  
+};
 
 export default ChatContainer;
